6ccacee57749445849ceede52fd17de75ca81253
Quienten <quientenmiller2@gmail.com>
2021-05-20 14:30:57 -0700

    Add blank about, faq, and contact page

--- covid_dashboard/dashboard/urls.py
+++ covid_dashboard/dashboard/urls.py
@@ -7,5 +7,8 @@ app_name = 'dashboard'
 
 urlpatterns = [
     # Home page
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('about', views.page, name='about'),
+    path('faq', views.page, name='faq'),
+    path('contact', views.page, name='contact')
 ]
--- covid_dashboard/dashboard/views.py
+++ covid_dashboard/dashboard/views.py
@@ -7,12 +7,16 @@ from . import maps, dataprocessing
 def index(request):
     """The main dashboard"""
     data = dataprocessing.world_map_data()
+
     countries = dataprocessing.left_table(data[0])
-    #print(countries)
     world_map = maps.world_map(data[0])
+
     context = {
-        'countries': countries,
-        'big_case_num': data[1],
-        'world_map': world_map
+       'countries': countries,
+       'big_case_num': data[1],
+       'world_map': world_map
     }
     return render(request, 'dashboard/index.html', context)
+
+def page(request):
+    return render(request, 'dashboard/' + request.path + '.html')
